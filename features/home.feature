a9632ca535400a84ccc46ca134521970a6c5613a
SilviadelPiano <silvia.delpiano27@gmail.com>
2019-09-02 07:48:19 +0200

    home page Cucumber test

---
+++ features/home.feature
@@ -0,0 +1,8 @@
+Feature: Go to the "SignUp" page from the "Home" page
+    As a User
+    So that I can sign sign up 
+
+        Scenario: 
+            Given I am on the home page
+            When I press Sign Up
+            Then I shoul be on the sign-up page
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -49,8 +49,12 @@ When /^(?:|I )go to (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
-When /^(?:|I )press "([^"]*)"$/ do |button|
+When /^(?:|I )press (.+)$/ do |button|
   click_button(button)
+end
+
+Then /^(?:|I )shoul be on (.+)$/ do |page_name|
+  visit path_to(page_name)
 end
 
 When /^(?:|I )follow "([^"]*)"$/ do |link|
@@ -252,3 +256,4 @@ end
 Then /^show me the page$/ do
   save_and_open_page
 end
+
